4d592423268c122a6ec7e7e721202484efc43080
Jonathan Hoyt <hoyt@github.com>
2012-04-09 14:24:04 -0400

    Updated VCR config.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,9 +5,9 @@ require 'webmock/rspec'
 require 'vcr'
 require 'ghee'
 
-VCR.config do |c|
+VCR.configure do |c|
   c.cassette_library_dir = File.expand_path('../responses', __FILE__)
-  c.stub_with :webmock
+  c.hook_into :webmock
   c.default_cassette_options = {:record => :once}
 end
 
