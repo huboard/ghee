c3cabb12d6b26cdb214aa116585f3606ca5c3a21
Stan Hu <stanhu@gmail.com>
2015-12-11 02:14:58 -0800

    Add issue and milestone translation

---
+++ lib/ghee/gitlab/api/translators/issue_translator.rb
@@ -0,0 +1,35 @@
+class Ghee
+  module GitLab
+    class IssueTranslator < Ghee::ApiTranslator
+      def translate_hash(input)
+        output = {}
+        store_passthrough(input, output)
+        store_remapped(input, output)
+        output
+      end
+
+      def store_passthrough(input, output)
+        attributes = %w(id title state assignee state updated_at created_at milestone)
+        attributes.each do |attr|
+          output[attr] = input[attr]
+        end
+      end
+
+      def store_remapped(input, output)
+        output['id'] = input['iid']
+        output['user'] = {}
+        output['user']['id'] = input['author']['id']
+        output['user']['login'] = input['author']['username']
+
+        output['milestone'] = nil
+
+        if input['milestone']
+          translator = MilestoneTranslator.new(:milestone)
+          output['milestone'] = translator.translate_hash(input['milestone'])
+        end
+
+        output
+      end
+    end
+  end
+end
