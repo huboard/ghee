9cd51622d2f76f9b68d92b1a9686eb783d149869
Stan Hu <stanhu@gmail.com>
2015-12-12 19:38:48 -0800

    Add support for GitLab milestones

---
+++ lib/ghee/gitlab/api/milestones.rb
@@ -0,0 +1,63 @@
+class Ghee
+
+  # API module encapsulates all of API endpoints
+  # implemented thus far
+  #
+  module API
+
+    module Repos
+
+      # The Milestones module handles all of the Github Repo Milestones
+      # API endpoints
+      #
+      module Milestones
+
+
+        # Gists::Proxy inherits from Ghee::Proxy and
+        # enables defining methods on the proxy object
+        #
+        class Proxy < ::Ghee::ResourceProxy
+          include Ghee::CUD
+
+          # Close milestone - closed milestone by id
+          #
+          # usage - ghee.repos("my_repo").milestones(1).close
+          #
+          # returns boolean
+          #
+          def close
+            connection.put(path_prefix,:state_event => "closed").status == 200
+          end
+
+          # Returns closed milestones
+          #
+          # Returns json
+          #
+          def closed
+            response = connection.get path_prefix do |req|
+              req.params["state"] = "closed"
+            end
+            response.body
+          end
+
+        end
+      end
+
+      # Gists::Proxy inherits from Ghee::Proxy and
+      # enables defining methods on the proxy object
+      #
+      class Proxy < ::Ghee::ResourceProxy
+
+        # Get milestones
+        #
+        # Returns json
+        #
+        def milestones(number=nil, params={})
+          params = number if number.is_a?Hash
+          prefix = (!number.is_a?(Hash) and number) ? "#{path_prefix}/milestones/#{number}" : "#{path_prefix}/milestones"
+          Ghee::API::Repos::Milestones::Proxy.new(connection, prefix, ::Ghee::GitLab::MilestoneTranslator.new(:milestone), params)
+        end
+      end
+    end
+  end
+end
